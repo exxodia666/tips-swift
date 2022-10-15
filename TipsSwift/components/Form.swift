a9f8a5d6317b96aaaaff641d1cccb29f9d6cd393
spike <ololmkbc.dev@gmail.com>
2022-10-16 02:07:16 +0300

    feat: added image picker

--- TipsSwift/components/Form.swift
+++ TipsSwift/components/Form.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct Form: View {
     @StateObject private var createTipViewModel: CreateTipViewModel = CreateTipViewModel()
+    
+    @State var showImagePicker: Bool = false
+    @State var image: Image? = nil
+    
     let showSheet: () -> Void
     init(showSheet: @escaping () -> Void) {
         UITextView.appearance().backgroundColor = .clear
@@ -68,35 +72,99 @@ struct Form: View {
             }
             .background(Color.light_peach)
             .foregroundColor(.white)
-            .frame(
-                alignment: .center
-            )
+            .frame(alignment: .center)
             .padding(.horizontal, 45)
             .overlay(
                 RoundedRectangle(cornerRadius: 15)
                     .strokeBorder(.white, lineWidth: 2)
                     .padding(.horizontal, 25)
             )
-            TextField(
-                "",
-                text: $createTipViewModel.image
-            )
-                .placeholder(when: createTipViewModel.image.isEmpty) {
-                    Text("Add Image (Optional)")
+            //            TextField(
+            //                "",
+            //                text: $createTipViewModel.image
+            //            )
+            //                .placeholder(when: createTipViewModel.image.isEmpty) {
+            //                    Text("Add Image (Optional)")
+            //                        .foregroundColor(.white)
+            //                        .font(.custom(Fonts.Montserrat.rawValue, size: 16))
+            //                }
+            //                .foregroundColor(.white)
+            //                .frame(
+            //                    height: 48.0,
+            //                    alignment: .center
+            //                )
+            //                .padding(.horizontal, 45)
+            //                .overlay(
+            //                    RoundedRectangle(cornerRadius: 15)
+            //                        .strokeBorder(.white, lineWidth: 2)
+            //                        .padding(.horizontal, 25)
+            //                )
+            //            ZStack {
+            //                VStack {
+            //                    Button(action: {
+            //                        self.showImagePicker.toggle()
+            //                    }) {
+            if image == nil {
+                Button(action: {
+                    self.showImagePicker.toggle()
+                }, label: {
+                    Spacer()
+                    Text("ADD IMAGE (OPTIONAL)")
                         .foregroundColor(.white)
-                        .font(.custom(Fonts.Montserrat.rawValue, size: 16))
-                }
-                .foregroundColor(.white)
-                .frame(
-                    height: 48.0,
-                    alignment: .center
-                )
-                .padding(.horizontal, 45)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .strokeBorder(.white, lineWidth: 2)
-                        .padding(.horizontal, 25)
-                )
+                        .font(.custom(Fonts.MontserratMedium.rawValue, size: 16))
+                    Spacer()
+                })
+                    .sheet(isPresented: $showImagePicker) {
+                        ImagePicker(sourceType: .photoLibrary) { image in
+                            self.image = Image(uiImage: image)
+                        }
+                    }
+                    .frame(
+                        height: 48.0,
+                        alignment: .center
+                    )
+                    .padding(.horizontal, 45)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .strokeBorder(.white, lineWidth: 2)
+                            .foregroundColor(.white)
+                    ).padding(.horizontal, 25)}
+            Spacer()
+            if image != nil {
+                ZStack {
+                    image?
+                        .resizable()
+                        .background(Color.light_peach)
+                        .padding(.horizontal, 24.0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 0)
+                                .strokeBorder(.white, lineWidth: 2)
+                                .padding(.horizontal, 24.0)
+                        )
+                    //                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button {
+                                image = nil
+                            } label: {
+                                Image(systemName: "trash")
+                                    .frame(width: 30, height: 30, alignment: .center)
+                                    .font(.system(size: 16))
+                                    .foregroundColor(.white)
+                                    .background(
+                                        Circle()
+                                            .size(width: 30, height: 30)
+                                            .foregroundColor(.red)
+                                    )
+                                    .padding(.trailing, 15)
+                                    .padding(.top, -2)
+                            }
+                        }
+                        Spacer()
+                    }
+                }}
+            
             //            TextField(
             //                "",
             //                text: $createTipViewModel.deadline
